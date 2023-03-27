6165af56778406b467dcfc5c5d97acd260f208f7
Martin Cantonny <cantonny.martin16@gmail.com>
2023-03-27 13:44:20 +0200

    .env

--- src/src/services/post.service.js
+++ src/src/services/post.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/posts/";
+const API_URL = `${process.env.REACT_APP_API_URL}/posts/`;
 
 const getPosts = async (post) => {
   if (post) {
